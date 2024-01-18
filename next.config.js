bf9ca9855dd67977d98570533a61ab483c512831
Debaraj Rout <debrajrout28@gmail.com>
2024-01-18 10:20:34 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["misc.scdn.co", "i.scdn.co", "geo-media.beatsource.com", "i1.sndcdn.com", "media.pitchfork.com", "seed-mix-image.spotifycdn.com", "tbvswgcbrilqlaxdsxii.supabase.co"]
+    domains: ["misc.scdn.co",
+      "i.scdn.co",
+      "geo-media.beatsource.com",
+      "i1.sndcdn.com",
+      "media.pitchfork.com",
+      "seed-mix-image.spotifycdn.com",
+      "tbvswgcbrilqlaxdsxii.supabase.co",
+      "svnrczyzztkwxeonbqel.supabase.co",]
   }
 }
 
